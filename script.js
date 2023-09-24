531599afa13f4c4ca6b86f7fdc00db2ef5b49290
santiago gutierrez <santymillan95@gmail.com>
2023-09-24 11:56:17 -0500

    lab7

---
+++ script.js
@@ -0,0 +1,31 @@
+// var divPrueba = document.getElementById("prueba123");
+// divPrueba.classList.add("carta--uno");
+// divPrueba.style.backgroundImage = "url('https://rickandmortyapi.com/api/character/avatar/1.jpeg')";
+// var texto = "OwO";
+
+
+// for (var i = 0; i < 10; i++) {
+    // var nuevoDiv = document.createElement("div"); 
+    // nuevoDiv.classList.add("carta--uno"); 
+    // nuevoDiv.innerHTML = texto; 
+    // divPrueba.appendChild(nuevoDiv); 
+// }
+
+// for (var i = 0; i < 10; i++) {
+//     var nuevoDiv = document.createElement("div"); 
+//     nuevoDiv.classList.add("carta--uno"); 
+//     nuevoDiv.style.backgroundImage = "url('https://rickandmortyapi.com/api/character/avatar/' + nuevoDiv +'.jpeg')";
+//     nuevoDiv.innerHTML = texto; 
+//     divPrueba.appendChild(nuevoDiv); 
+//  }
+
+// var container = document.getElementNyID("contenedor")
+// container.classList.add("container") 
+
+
+let numero = 0;
+for(numero= 0; numero < 10; numero++){
+    
+}
+
+
